1daabf10663b6834af129ae597e5aeb226d76db3
sunye <franksunye@hotmail.com>
2024-04-18 13:11:31 +0800

    Minor changes for home, experiement the code and design.

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	title: `LeadGen GPT | Simple & Powerful AI-Powered Lead Generation & Management for OpenAI's GPTs`,
+	description: `LeadGen GPT offers a seamless solution for businesses harnessing the power of OpenAI's GPTs to effortlessly generate and manage leads. Our platform leverages advanced AI technology to streamline the process, making it intuitive and user-friendly. With LeadGen GPT, you can efficiently analyze customer data, respond promptly to inquiries, and optimize your lead generation strategy with ease. Simplify your workflow and maximize your results with LeadGen GPT today!`,
+	url: `https://leadgen-gpt.vercel.app`, // No trailing slash!
+	name: `LeadGen-GPT`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
   showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
