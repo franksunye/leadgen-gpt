583431a6a13e4fb7b105b33d17c2a0484f952a62
sunye <franksunye@hotmail.com>
2024-04-18 16:55:32 +0800

    Simplify the navagtion.

--- src/config/nav.js
+++ src/config/nav.js
@@ -1,18 +1,6 @@
 export const nav = [
 	{
-		title: 'Home',
-		slug: '/',
-	},
-	{
 		title: 'Blog',
 		slug: '/blog',
-	},
-	{
-		title: 'About',
-		slug: '/company/about',
-	},
-	{
-		title: 'Contact',
-		slug: '/company/contact',
 	},
 ];
