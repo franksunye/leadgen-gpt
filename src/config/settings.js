96313b46fe1d26241707a40e6ddb46ebd5e2eac4
franksunye <sunye@parllay.cn>
2024-04-18 03:26:56 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/config/settings.js
@@ -0,0 +1,8 @@
+export default {
+	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
+	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
+	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
+	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+  enableThemeSwitcher: true,
+  showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+};
