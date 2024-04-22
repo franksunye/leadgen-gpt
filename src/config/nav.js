f299781e92a704d9ecbd081adf19bf86fa25b109
sunye <franksunye@hotmail.com>
2024-04-22 16:35:53 +0800

    Center our nav.

--- src/config/nav.js
+++ src/config/nav.js
@@ -1,5 +1,17 @@
 export const nav = [
 	{
+		title: 'Why',
+		slug: '/#Why',
+	},
+	{
+		title: 'How',
+		slug: '/#Steps',
+	},
+	{
+		title: 'Pricing',
+		slug: '/#Pricing',
+	},
+	{
 		title: 'Blog',
 		slug: '/blog',
 	},
