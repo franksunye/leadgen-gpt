3b2051ad05a3343290fd4169b347dad5982edb33
sunye <franksunye@hotmail.com>
2024-04-25 16:16:26 +0800

    Own social image.

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `LeadGen GPT | Simple & Powerful AI-Powered Lead Generation & Management Tool for OpenAI's GPTs`,
 	description: `LeadGen GPT offers a seamless way for businesses harnessing the power of OpenAI's GPTs to effortlessly generate and manage leads. Our platform leverages advanced AI technology to streamline the process, making it intuitive and user-friendly. With LeadGen GPT, you can efficiently analyze customer data, respond promptly to inquiries, and optimize your lead generation strategy with ease. Simplify your workflow and maximize your results with LeadGen GPT today!`,
-	url: `https://leadgen-gpt.vercel.app`, // No trailing slash!
+	url: `https://www.leadgen-gpt.com`, // No trailing slash!
 	name: `LeadGen-GPT`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
   showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
